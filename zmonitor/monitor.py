4b313096be86b64f2b09a77fd16391dc81c70abb
Oz Elentok <oz.elen@gmail.com>
2016-06-24 21:48:00 +0300

    Refactor monitor.py to monitor.py to include the engine and monitors.py to include the different monitors

--- zmonitor/monitor.py
+++ zmonitor/monitor.py
@@ -1,6 +1,3 @@
-from abc import ABC, abstractmethod
-import glob
-import os
 import asyncio
 import threading
 import logging
@@ -9,95 +6,11 @@ import json
 from django.utils import timezone
 from channels import Group
 from . import settings
+from . import monitors
 from .models import MonitorItem
 from .rest import MonitorItemSerializer
 
 log = logging.getLogger('zmonitor')
-
-class BaseMonitor(ABC):
-
-    def __init__(self, name):
-        self.name = name
-        self.status = ''
-
-    @abstractmethod
-    def check(self):
-        pass
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, value):
-        self._status = value
-
-class FileExistenceMonitor(BaseMonitor):
-
-    def __init__(self, name, glob_patterns):
-        super().__init__(name)
-        self._glob_patterns = glob_patterns
-        self.last_file_path = None
-
-    @property
-    def last_file_path(self):
-        return self._last_file_path
-
-    @last_file_path.setter
-    def last_file_path(self, value):
-        self._last_file_path = value
-
-    def get_newest_file_paths(self):
-        files_found = []
-        for glob_pattern in self._glob_patterns:
-            files_found += glob.glob(glob_pattern)
-        files_found.sort(key=os.path.getmtime)
-        return files_found
-
-    def check(self):
-        files_found = self.get_newest_file_paths()
-        if len(files_found) == 0:
-            return False
-        if files_found[-1] == self.last_file_path:
-            return False
-        self.last_file_path = files_found[-1]
-        self.status = 'File Arrived'
-        return True
-
-class LogLinesMonitor(FileExistenceMonitor):
-
-    def __init__(self, name, glob_patterns, lines_meanings):
-        super().__init__(name, glob_patterns)
-        self._lines_meanings = lines_meanings
-        self._current_log_path = None
-
-    def check(self):
-        files_found = self.get_newest_file_paths()
-        if len(files_found) == 0:
-            return False
-        if files_found[-1] != self.last_file_path:
-            self._lines_read = 0
-            self.last_file_path = files_found[-1]
-
-        was_updated = False
-        with open(self.last_file_path, 'r') as log_file:
-            for line_num, line in enumerate(log_file):
-                if line_num < self._lines_read:
-                    continue
-                for line_meaning in self._lines_meanings:
-                    if line_meaning[0] in line:
-                        was_updated = True
-                        self.status = line_meaning[1]
-            self._lines_read = line_num
-        return was_updated
 
 class MonitorEngine:
 
@@ -105,10 +18,11 @@ class MonitorEngine:
 
     def register_monitor(self, monitor_config):
         try:
-            monitor_class = globals()[monitor_config['monitor']]
-            monitor = monitor_class(monitor_config['name'], *monitor_config['monitor_params'])
+            monitor_class = getattr(monitors, monitor_config['monitor'])
+            monitor_name = monitor_config['name']
+            monitor = monitor_class(monitor_name, *monitor_config['monitor_params'])
             monitor_item, created = MonitorItem.objects.get_or_create(
-                    name=monitor_config['name'],
+                    name=monitor_name,
                     defaults={
                         'source': monitor_config['source'],
                         'description': monitor_config['description'],
