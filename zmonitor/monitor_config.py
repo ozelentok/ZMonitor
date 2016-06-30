e51b4dd0d01404a23be621d3dabd36908b1227e1
Oz Elentok <oz.elen@gmail.com>
2016-07-01 01:08:45 +0300

    Add colors to date views

--- zmonitor/monitor_config.py
+++ zmonitor/monitor_config.py
@@ -14,9 +14,27 @@ config = [
             'name': 'Waka log file',
             'source': 'Local Computer',
             'description': 'Crazy log file',
-            'arrival_interval': timedelta(seconds=20),
+            'arrival_interval': timedelta(seconds=60),
             'is_active': True,
             'monitor': 'LogLinesMonitor',
             'monitor_params': (['/tmp/$DATE/waka*.txt'], [('quack', 'Good a duck was here'), ('meow', 'Bad a cat was here')],)
-        }
+        },
+        {
+            'name': '/var/log files',
+            'source': 'Local Computer',
+            'description': 'Log files',
+            'arrival_interval': timedelta(seconds=120),
+            'is_active': True,
+            'monitor': 'FileExistenceMonitor',
+            'monitor_params': (['/var/log/*'],)
+        },
+        {
+            'name': '/home/oz files',
+            'source': 'Local Computer',
+            'description': 'Home files',
+            'arrival_interval': timedelta(days=1),
+            'is_active': True,
+            'monitor': 'FileExistenceMonitor',
+            'monitor_params': (['/home/oz/*'],)
+        },
 ]
--- zmonitor/static/zmonitor/js/views.js
+++ zmonitor/static/zmonitor/js/views.js
@@ -6,17 +6,22 @@ var RootLayout = Mn.LayoutView.extend({
 	},
 });
 
-Backbone.Stickit.addHandler({
+var OpacityViewHandler = {
 	selector: 'td',
 	afterUpdate: function($el) {
 		$el.css('opacity', 0.1);
 		$el.animate({'opacity': 1}, 1200);
 	},
-});
+};
+
+Backbone.Stickit.addHandler(OpacityViewHandler);
 
 var MonitorItemView = Mn.ItemView.extend({
 	tagName: 'tr',
 	template: '#template-monitor-item',
+	ui: {
+		'itemLastArrival': '.monitor-item-last-arrival',
+	},
 	bindings: {
 		'.monitor-item-pk': 'pk',
 		'.monitor-item-name': 'name',
@@ -31,6 +36,7 @@ var MonitorItemView = Mn.ItemView.extend({
 				}
 				return 'Did not arrive';
 			},
+			afterUpdate: 'onNewArrival',
 		},
 		'.monitor-item-last-update': {
 			observe: ['last_update'],
@@ -40,11 +46,47 @@ var MonitorItemView = Mn.ItemView.extend({
 				}
 				return 'No update';
 			},
+			afterUpdate: 'onNewUpdate',
 		},
 		'.monitor-item-arrival-interval': 'arrival_interval',
 	},
 	onRender: function() {
 		this.stickit();
+	},
+	onNewArrival: function($el) {
+		OpacityViewHandler.afterUpdate($el);
+		var lastArrival = moment(this.model.get('last_arrival'));
+		var msDiff = Date.now() - lastArrival;
+		var timeIntervalMs = this.timeIntervalToMs(this.model.get('arrival_interval'));
+		if (msDiff <= timeIntervalMs) {
+			$el.css('color', '#0099FF');
+		} else if (msDiff <= 2 * timeIntervalMs) {
+			$el.css('color', '#99FF00');
+		} else {
+			$el.css('color', '#FF3300');
+		}
+	},
+	onNewUpdate: function($el) {
+		OpacityViewHandler.afterUpdate($el);
+		var lastUpdate = moment(this.model.get('last_update'));
+		var msDiff = Date.now() - lastUpdate;
+		if (msDiff <= 5 * 60 * 1000) {
+			$el.css('color', '#0099FF');
+		} else {
+			$el.css('color', '#FF3300');
+		}
+		this.onNewArrival(this.ui.itemLastArrival);
+	},
+	timeIntervalToMs: function(timeDiff) {
+		var timePattern = /(\d{2}):(\d{2}):(\d{2})/;
+		var patternMatch = timePattern.exec(timeDiff);
+		if (patternMatch === null) {
+			throw Error('String does not contain a time difference');
+		}
+		var totalSeconds = parseInt(patternMatch[3], 10);
+		totalSeconds += parseInt(patternMatch[2], 10) * 60;
+		totalSeconds += parseInt(patternMatch[1], 10) * 3600;
+		return totalSeconds * 1000;
 	},
 	formatDateTime: function(dateTimeValue) {
 		return moment(dateTimeValue).format('YYYY-MM-DD HH:mm:ss ZZ');
